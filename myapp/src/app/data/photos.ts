e89dae880bb90029da5e1f56df95abbd54ca4024
Tengis <apoxmn@gmail.com>
2025-10-14 16:42:15 +0800

    update

---
+++ myapp/src/app/data/photos.ts
@@ -0,0 +1,8 @@
+ export const photos = [
+ { id: "mountain", src: "/images/cs2.png", 
+desc: "Beautiful mountain" },
+ { id: "city", src: "/images/cs2.png", desc: 
+"Busy city life" },
+ { id: "nature", src: "/images/cs2.png", 
+desc: "Peaceful nature" },
+ ];
---
+++ myapp/src/app/gallery/page.tsx
@@ -0,0 +1,20 @@
+ import Image from "next/image";
+ import { photos } from "@/app/data/photos";
+ export default function GalleryPage() {
+ return (
+ <div>
+ <h1>Photo Gallery</h1>
+ <div style={{ display: "flex", gap: "20px" }}>
+ {photos.map((photo) => (
+ <div key={photo.id}>
+ <Image src={photo.src} alt={photo.desc}
+ width={300} height={200} placeholder="blur"
+ blurDataURL="/images/placeholder.png"
+ />
+ <p>{photo.desc}</p>
+ </div>
+ ))}
+ </div>
+ </div>
+    )
+ }
